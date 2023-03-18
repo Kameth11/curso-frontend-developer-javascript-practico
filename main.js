77f0e1c41c0965560b206539cdd9f135b7dd736a
Kameth11 <matidv3@gmail.com>
2023-03-18 17:21:08 -0300

    Se reestructuraron nuevas clases con html nuevo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const menuEmail = document.querySelector(".navbar-email");
 const menuHamIcon = document.querySelector(".menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const menuCartIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
 const desktopMenu = document.querySelector(".desktop-menu");
@@ -13,16 +13,16 @@ menuCartIcon.addEventListener("click", toggleCartAside);
 function toggleDesktopMenu() {
   //aside y desktopmenu tengo que hacer que cuando apreto uno tengo que sacar el otro.
   desktopMenu.classList.toggle("inactive");
-  aside.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
 }
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
-  aside.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
 }
 function toggleCartAside() {
   mobileMenu.classList.add("inactive");
   desktopMenu.classList.add("inactive");
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
